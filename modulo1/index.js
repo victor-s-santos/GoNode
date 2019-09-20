6d09f4547e62757881c98289c5131c0da337fb08
victor-s-santos <victor.s.silva@unesp.br>
2019-09-20 16:02:05 -0300

    criando um servidor muito simples

---
+++ modulo1/index.js
@@ -0,0 +1,7 @@
+const http = require('http')
+//usado para criar servidores simples
+
+http.createServer((req, res) => {
+    console.log(req);
+    return res.end("Hello world in a web page!")
+}).listen(3000);
