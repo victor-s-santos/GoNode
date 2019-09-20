98e1e744c937ef7da5f1b1f3d3dfbc5abe6dea64
victor-s-santos <victor.s.silva@unesp.br>
2019-09-20 16:08:02 -0300

    editando gitignore

--- modulo1/index.js
+++ modulo1/index.js
@@ -1,7 +1,9 @@
 const http = require('http')
 //usado para criar servidores simples
 
-http.createServer((req, res) => {
-    console.log(req);
-    return res.end("Hello world in a web page!")
-}).listen(3000);
+http
+    .createServer((req, res) => {
+        console.log(req);
+        return res.end("Hello world in a web page!")
+    })
+    .listen(3000);
