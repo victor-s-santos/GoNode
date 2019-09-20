7e445f57a71a5118d1876d1e9e8aaa6b0967b817
victor-s-santos <victor.s.silva@unesp.br>
2019-09-20 17:22:03 -0300

    usando o nunjucks para renderizar javascript como template

--- modulo1/index.js
+++ modulo1/index.js
@@ -1,30 +1,18 @@
 const express = require("express");
+const nunjucks = require('nunjucks');
 
 const app = express();
 
-const logMiddleware = (req, res, next) => {
-    console.log(
-        `HOST: ${req.headers.host},
-         URL: ${req.url},
-         METHOD: ${req.method}`
-    );
-    return next();
-    //desta forma, em todas as requisições este console será retornado
-};
-
-app.use(logMiddleware);
+nunjucks.configure('minhasviews', {
+    autoescape: true,
+    express: app,
+    watch: true,
+});
+//definindo extensão dos arquivos views
+app.set('view engine', 'njk');
 
 app.get('/', (req, res) => {
-    return res.send(`Hello ${req.query.name}`);
+    return res.render('list', { nome: 'Victor'});
 });
 
-app.get('/login/:nickname', (req, res) => {
-    return res.send(`Bem vindo, ${req.params.nickname}`);
-});
-
-app.get('/conta/:nome', (req, res) => {
-    return res.json({
-        message: `Bem vindo ${req.params.nome}, como vai você?`
-    });
-})
 app.listen(3000);
