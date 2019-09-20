240c7c30b9041ae999a21189807ae4d67cb38125
victor-s-santos <victor.s.silva@unesp.br>
2019-09-20 18:03:25 -0300

    navegacao entre as paginas pelo node

--- modulo1/index.js
+++ modulo1/index.js
@@ -11,8 +11,13 @@ nunjucks.configure('minhasviews', {
 //definindo extensão dos arquivos views
 app.set('view engine', 'njk');
 
+const usuarios = ['Suelen Simões Costa', 'Victor Santos Silva', 'Noob Saibot']
 app.get('/', (req, res) => {
-    return res.render('list', { nome: 'Victor'});
+    return res.render('list', { nome: 'Victor', usuarios });
 });
+
+app.get('/new', (req, res) => {
+    return res.render('new');
+})
 
 app.listen(3000);
