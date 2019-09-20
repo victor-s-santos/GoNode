3a47d1ec5aee6b10f0de4e76207d9b65f98a939e
victor-s-santos <victor.s.silva@unesp.br>
2019-09-20 16:23:28 -0300

    retornando como json

--- modulo1/index.js
+++ modulo1/index.js
@@ -10,4 +10,9 @@ app.get('/login/:nickname', (req, res) => {
     return res.send(`Bem vindo, ${req.params.nickname}`);
 });
 
+app.get('/conta/:nome', (req, res) => {
+    return res.json({
+        message: `Bem vindo ${req.params.nome}, como vai você?`
+    });
+})
 app.listen(3000);
